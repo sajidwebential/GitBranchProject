4bd1903f7d0fddfb0f92e37fcab2b900ba3609ae
hardik <hardikkanzariya.datatech@gmail.com>
2024-04-10 10:52:41 +0530

    first commit in master

--- app/src/main/java/com/sniffspace/gitbranchproject/MainActivity.kt
+++ app/src/main/java/com/sniffspace/gitbranchproject/MainActivity.kt
@@ -8,4 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun masterFunction() {
+        // comment for master
+    }
 }
